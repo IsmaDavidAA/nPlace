8e7f11fed96f93c49b78687c25787e7c4ad75011
IsmaDavidAA <idaasannmvgd@gmail.com>
2022-03-20 21:20:17 -0400

    add router

--- src/App.js
+++ src/App.js
@@ -3,25 +3,32 @@ import TopBar from "./components/TopBar/TopBar";
 import BuildingDetail from "./pages/BuildingDetail/BuildingDetail";
 import Home from "./pages/Home/Home";
 import SignUpSecond from "./pages/SignUp/SignUpSecond";
+import SignUp from "./pages/SignUp/SignUp";
 import ChatFeed from "./pages/Chat/ChatFeed";
 import ListApartmentBuilding from "./pages/ListApartmentBuilding/ListApartmentBuilding";
 import MyProperties from "./pages/MyProperties/MyProperties";
 import ConfirmIdentity from "./pages/ConfirmIdentity/ConfirmIdentity";
 import MyProfileInquilino from "./pages/MyProfileInquilino/MyProfileInquilino";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <TopBar />
-      {/* <SignUp /> */}
-      {/* <SignUpSecond /> */}
-      {/* <Home /> */}
-      {/* <BuildingDetail /> */}
-      {/* <ChatFeed /> */}
-      {/* <ListApartmentBuilding /> */}
-      {/* <MyProperties /> */}
-      {/* <ConfirmIdentity /> */}
-      <MyProfileInquilino />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signupconfirmation" element={<SignUpSecond />} />
+        <Route path="buildingdetail" element={<BuildingDetail />} />
+        <Route path="chat" element={<ChatFeed />} />
+        <Route
+          path="listapartmentbuilding"
+          element={<ListApartmentBuilding />}
+        />
+        <Route path="myproperties" element={<MyProperties />} />
+        <Route path="confirmidentity" element={<ConfirmIdentity />} />
+        <Route path="myprofileinquilino" element={<MyProfileInquilino />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
